6f80b7b9a8e6f9eaeda00a781157704523f04e4f
michfarr <etnunc@protonmail.com>
2016-04-26 10:07:08 +0200

    add Course component to react

--- REACT/src/App.js
+++ REACT/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Courses from './Courses';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello World!</h1>
+            <Courses />
         );
     }
 }
---
+++ REACT/src/Courses.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Courses extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            courses: [
+                { name: "React", description: "Its a course" },
+                { name: "Javascript", description: "Learn the basics" }
+            ]
+        };
+    }
+
+    compononentDidMount() {
+        this.setState({
+            courses: []
+        });
+    }
+
+    render() {
+        let courses = this.state.courses.map(function(course) {
+            return <li key={course.id}><h1>{course.name}</h1><p>{course.description}</p></li>
+        });
+
+        return (
+            <ul>
+                {courses}
+            </ul>
+        )
+    }
+}
+
+export default Courses;
